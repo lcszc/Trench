0358b0a5dd9690c61bba1e2141e5208046661a33
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-06 19:44:49 -0300

    Update Trench to make it a root project.

---
+++ buildSrc/src/main/java/config/GradlePlugins.kt
@@ -0,0 +1,6 @@
+package config
+
+object GradlePlugins {
+    val android = "com.android.tools.build:gradle:3.6.3"
+    val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61"
+}
---
+++ buildSrc/src/main/java/config/Trench.kt
@@ -0,0 +1,9 @@
+package config
+
+object Trench {
+    object Sdk {
+        val min = 16
+        val target = 29
+        val compile = 29
+    }
+}
