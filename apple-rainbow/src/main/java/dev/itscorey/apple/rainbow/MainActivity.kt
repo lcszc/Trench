5d88ec4cc8070a3c25cb18d0019b1b6bfd97482b
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-06 23:16:22 -0300

    (Apple Rainbow): Remove android.os.Handler usage in favor of animator's startOffset.

--- apple-rainbow/src/main/java/dev/itscorey/apple/rainbow/MainActivity.kt
+++ apple-rainbow/src/main/java/dev/itscorey/apple/rainbow/MainActivity.kt
@@ -1,18 +1,23 @@
 package dev.itscorey.apple.rainbow
 
 import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : AppCompatActivity(R.layout.main) {
     override fun onResume() {
         super.onResume()
-        Handler().postDelayed({
-            rainbow.animate().setDuration(100).alpha(1f).start()
-            val animatable = rainbow.drawable as Animatable
-            animatable.start()
-        }, 500)
+        /* (R.integer.rainbow_start_delay_anim_time = 500) + 100) */
+        /* This ensure that the fade starts to takes effect when the morphing starts */
+        /* Remember that the morphing will have a start delay of 500ms */
+        rainbow.animate().setDuration(600).alpha(1F).start()
+
+        val rainbowDrawable = rainbow.drawable as Animatable2Compat
+        rainbowDrawable.start()
     }
 }
